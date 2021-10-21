9e7a8711976d3ae0c9332d7fd3b56a945052686f
Guo Zhengjin <guo.zhengjin@gmail.com>
2021-10-21 16:10:19 +0800

    first commit

---
+++ src/main/java/com/zhengjin/guo/projectsboot/ProjectSbootApplication.java
@@ -0,0 +1,13 @@
+package com.zhengjin.guo.projectsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhengjin/guo/projectsboot/ProjectSbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhengjin.guo.projectsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
